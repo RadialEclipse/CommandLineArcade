31bf095ea68a6dee8e292328945316c75c4b492a
LTDev1337 <xhogan1337@gmail.com>
2026-07-01 13:18:56 -0500

    updated .gitignore to ignore any .ipch files

--- CommandLineArcade/CommandLineArcade/helloWorld.h
+++ CommandLineArcade/CommandLineArcade/helloWorld.h
@@ -1,39 +1,53 @@
 #pragma once
 #include <iostream>
+#include <chrono>
+#include <ctime>
+#include <thread>
 
 class HelloWorld
 {
 	public:
-		int num = 0;
+		int num = 0 ;
+		
 
 		HelloWorld(int num)
 		{
-			this->num = num;
+			std::cout << "Contructor created successfully!\n";
+			//this->num = num;
 			
-			//addNum();
-			//std::cout << num << std::endl;
-
 		}
 		~HelloWorld()
 		{
 			std::cout << "The constructor was destroyed!" << std::endl;
+			std::cout << "\nProgram terminated.";
 		}
 
-		/*void addNum(int num)
-		{
-			int result = num + 5;
-		}*/
-
-		void printNum()
+		void printNum(int result)
 		{
 			std::cout << num << std::endl;
+			std::cout << "The result was: " << result << "\nThis program will cease in : \n";
 		}
 
-		void addNum(int numTwo)
+		void addNum(int num,int numTwo)
 		{
-			num += numTwo;
-
+			int result = num += numTwo;
+			printNum(result);
 			
+		}
+
+		void timer(int seconds)
+		{
+			seconds = seconds;
+
+			while (seconds >= 1)
+			{
+				std::cout << seconds << "...\n";
+
+				std::this_thread::sleep_for(std::chrono::seconds(1));
+
+				seconds--;
+
+			}
 		}
 	
 };
--- CommandLineArcade/CommandLineArcade/main.cpp
+++ CommandLineArcade/CommandLineArcade/main.cpp
@@ -5,9 +5,19 @@
 
 int main()
 {
-	HelloWorld var(5);
-	var.addNum(5);
-	var.printNum();
+	int inputNum;
+	int secondNum;
 
+	std::cout << "Please enter a number.\n";
+	std::cin >> inputNum;
+
+	std::cout << "Please enter a second number for addition.\n";
+	std::cin >> secondNum;
+
+	HelloWorld var(inputNum);
+	var.addNum(inputNum,secondNum);
+	//var.printNum();
+	var.timer(3);
+	//var.~HelloWorld();
 	return 0;
 }
